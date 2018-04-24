76d1a01b5eddc921e97cb9b003528508c0e96b4d
elinori <38704769+elinori@users.noreply.github.com>
2018-04-25 02:27:10 +0300

    my project

--- app.module.ts
+++ app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { DataService } from './data.service';
-import{BooksServiceService} from './books-service.service';
+import {BooksServiceService} from './books-service.service';
 import { AppComponent } from './app.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { FilterPipe } from './filter.pipe';
@@ -19,10 +19,10 @@ import { FilterPipe } from './filter.pipe';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-ReactiveFormsModule,    
+ReactiveFormsModule,
  HttpClientInMemoryWebApiModule.forRoot(  DataService, { dataEncapsulation: false }),
   ],
-  providers: [DataService,BooksServiceService],
+  providers: [DataService, BooksServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
